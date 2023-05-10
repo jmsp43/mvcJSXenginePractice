b2ac77e33cb752b57d40ad58718b08e8337763cb
Jenna Perez <jmsp43@gmail.com>
2023-05-10 14:29:53 -0400

    added fakerJS data

---
+++ models/people.js
@@ -0,0 +1,148 @@
+const { faker } = require('@faker-js/faker');
+
+const people = [
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    },
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        birthday: faker.date.birthdate(),
+        phone: faker.phone.phone(),
+        gender: faker.name.gender(),
+        job: faker.name.jobTitle()
+    }
+
+
+]
+
+module.exports = people
